94013779f6841dbcdd6c1f09cdd17b163dfd3b3f
Grzegorz Leśniowski <vproject.111@gmail.com>
2026-01-31 18:09:48 +0100

    release: v3.6.0 - RAE Mesh (Federated Memory)

--- rae-core/rae_core/version.py
+++ rae-core/rae_core/version.py
@@ -1,6 +1,6 @@
 """Version information for RAE-core."""
 
-__version__ = "3.5.0"
+__version__ = "3.6.0"
 VERSION = __version__
 __author__ = "Grzegorz Leśniowski"
 __email__ = "contact@dreamsoft.pro"
